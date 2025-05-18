a8a677c7de7ecf56d805185589aeee6cda0d724f
Serhiy Tatomyre <combat.ygar@gmail.com>
2025-05-18 11:14:42 +0300

    first work

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,17 @@
-const generateContacts = async (number) => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+
+const generateContacts = async (number) => {
+  try {
+    const contacts = await readContacts();
+    const newContacts = Array.from({ length: number }, createFakeContact);
+    contacts.push(...newContacts);
+    await writeContacts(contacts);
+    console.log(`Generated ${number} new contacts`);
+  } catch (error) {
+    console.error('Error generating contacts:', error);
+  }
+};
 
 generateContacts(5);
